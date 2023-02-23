55edeee3579d68aed0903fa866e70586c6a52c5d
vaibhav <vaibhavmehta524@gmail.com>
2023-02-24 05:01:06 +0530

    Modified to Find Maximum Integer

---
+++ Generic/FindMaximum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class FindMaximum
+    {
+            public void FindMaxInt(int firstInt, int secondInt, int thirdInt)
+            {
+                if (firstInt.CompareTo(secondInt) > 0 && firstInt.CompareTo(thirdInt) > 0)
+                {
+                    Console.WriteLine("The {0} is greatest of all number", firstInt);
+                }
+                else if (secondInt.CompareTo(thirdInt) > 0)
+                {
+                    Console.WriteLine("The {0} is greatest of all number", secondInt);
+                }
+                else
+                {
+                    Console.WriteLine("The {0} is gretest of all number", thirdInt);
+                }
+            }
+        }
+    }
+
---
+++ Generic/Program.cs
@@ -0,0 +1,26 @@
+﻿using Generic;
+using System;
+
+namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to find maximum using generics");
+            Console.WriteLine("Please enter an option");
+            Console.WriteLine("1:Find maximum integer");
+            int option = Convert.ToInt32(Console.ReadLine());
+            FindMaximum findMaximum = new FindMaximum();
+            switch (option)
+            {
+                case 1:
+                    findMaximum.FindMaxInt(12, 36, 46);
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
+        }
+    }
+}
