eacb609ea4b8132a36a338af0be4a30e916b6ac9
vaibhav <vaibhavmehta524@gmail.com>
2023-02-24 06:27:56 +0530

    Modified to Refactor for find maximum using generic

---
+++ Generic/FindMaxGeneric.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class FindMaxGeneric<T> where T : IComparable
+    {
+        public T GenericMaxNumber(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                Console.WriteLine("The maximum value is: " + firstValue);
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(thirdValue) > 0)
+            {
+                Console.WriteLine("The maximum value is: " + secondValue);
+                return secondValue;
+            }
+            else
+            {
+                Console.WriteLine("The maximum value is: " + thirdValue);
+                return thirdValue;
+            }
+        }
+    }
+}
--- Generic/Program.cs
+++ Generic/Program.cs
@@ -9,9 +9,12 @@ namespace Generic
         {
             Console.WriteLine("Welcome to find maximum using generics");
             Console.WriteLine("Please enter an option");
-            Console.WriteLine("1:Find maximum integer\n2:Find maximum float\n3:Find maximum string");
+            Console.WriteLine("1:Find maximum integer\n2:Find maximum float\n3:Find maximum string\n4:Find maximum using Generic");
             int option = Convert.ToInt32(Console.ReadLine());
             FindMaximum findMaximum = new FindMaximum();
+            FindMaxGeneric<int> findMaxInt = new FindMaxGeneric<int>();
+            FindMaxGeneric<float> findMaxFloat = new FindMaxGeneric<float>();
+            FindMaxGeneric<string> findMaxString = new FindMaxGeneric<string>();
             switch (option)
             {
                 case 1:
@@ -22,6 +25,11 @@ namespace Generic
                     break;
                 case 3:
                     findMaximum.FindMaxString("Apple", "Banana", "Pear");
+                    break;
+                case 4:
+                    findMaxInt.GenericMaxNumber(12, 25, 30);
+                    findMaxFloat.GenericMaxNumber(26.5f, 30.56f, 45.30f);
+                    findMaxString.GenericMaxNumber("Apple", "Pear", "Banana");
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
